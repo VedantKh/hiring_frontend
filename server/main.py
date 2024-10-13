4d29a51866c904328ac74d53708675b5e90a209c
coderinblack08 <kevinlu.email@gmail.com>
2024-10-12 21:22:32 -0700

    feat: change cors origin

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(
     app,
     supports_credentials=True,
-    resources={r"/*": {"origins": ["http://localhost:3000"]}},
+    resources={r"/*": {"origins": ["*"]}},
 )
 
 HEADERS = {
